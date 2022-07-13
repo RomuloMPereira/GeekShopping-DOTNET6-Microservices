ac88ed07024c9e3585ab9c9acf0e16b2eb09e82f
ROMULO\mprom <mp.romulo@gmail.com>
2022-07-13 16:39:11 -0300

    Configure the AutoMapper on ProductAPI

--- GeekShopping.ProductAPI/Startup.cs
+++ GeekShopping.ProductAPI/Startup.cs
@@ -1,4 +1,6 @@
-﻿using GeekShopping.ProductAPI.Model.Context;
+﻿using AutoMapper;
+using GeekShopping.ProductAPI.Config;
+using GeekShopping.ProductAPI.Model.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
@@ -22,6 +24,10 @@ namespace GeekShopping.ProductAPI
                 .UseMySql(connection,
                         new MySqlServerVersion(
                             new Version(8, 0, 29))));
+
+            IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+            services.AddSingleton(mapper);
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
